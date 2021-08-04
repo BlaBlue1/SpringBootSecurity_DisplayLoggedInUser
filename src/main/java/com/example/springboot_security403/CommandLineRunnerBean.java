a24f3830f9c3109dc128da22d0699752ffa5bf73
RuthWudu <ruthwudu576@gmail.com>
2021-08-04 10:45:13 -0400

    display logged in user

--- src/main/java/com/example/springboot_security403/CommandLineRunnerBean.java
+++ src/main/java/com/example/springboot_security403/CommandLineRunnerBean.java
@@ -15,16 +15,16 @@ public class CommandLineRunnerBean implements CommandLineRunner {
     RoleRepository roleRepository;
 
     public void run(String...args){
-        User user = new User("bethel", "bethel@domain.com", "bethel", "Bethel", "Sahle", true); //the names and passwords are examples
-        Role userRole = new Role("bethel", "ROLE_USER");
+        User user = new User("bart", "bart@domain.com", "bart", "Bart", "Simpson", true); //the names and passwords are examples
+        Role userRole = new Role("bart", "ROLE_USER");
 
         userRepository.save(user);
         roleRepository.save(userRole);
 
 
-        User admin = new User("elda", "elda@domain.com", "elda", "Elda", "Sahle", true);
-        Role adminRole1 = new Role("elda", "ROLE_ADMIN");
-        Role adminRole2 = new Role("elsa", "ROLE_USER");
+        User admin = new User("super", "super@domain.com", "super", "Super", "Hero", true);
+        Role adminRole1 = new Role("super", "ROLE_ADMIN");
+        Role adminRole2 = new Role("super", "ROLE_USER");
 
         userRepository.save(admin);
         roleRepository.save(adminRole1);
