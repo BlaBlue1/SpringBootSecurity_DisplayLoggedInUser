be9d0f9f7f5fadc90b358327c0cb860ea2160e9b
RuthWudu <ruthwudu576@gmail.com>
2021-08-04 09:33:08 -0400

    Spring Boot security405

--- src/main/java/com/example/springboot_security403/HomeController.java
+++ src/main/java/com/example/springboot_security403/HomeController.java
@@ -1,10 +1,23 @@
 package com.example.springboot_security403;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
+    }
 
     @RequestMapping("/")
     public String index() {
